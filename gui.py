f67062348caef0f6122ab4a947a28c9a87f9c199
Baksteennz <christoeras@gmail.com>
2023-09-04 12:38:08 +1200

    added linux support and a quit button

--- gui.py
+++ gui.py
@@ -1,5 +1,6 @@
 from tkinter import Tk, filedialog, Label, Button, Entry, Checkbutton
 import subprocess
+import platform
 
 class GUI:
     def __init__(self) -> None:
@@ -18,13 +19,22 @@ movie: bool = 0
 nameEntry = Entry(window, width=7, textvariable="")
 
 
-def submit():
+def pyCalc(os: str)-> str:
+   match os:
+        case "Windows":
+           return "python"
+        case "Linux":
+           return "python3"
+
+def submit()-> None:
     path = c.getPath()
     name = nameEntry.get()
+    pyver = pyCalc(platform.system())
+
     if movie == True:
-        dump = subprocess.run(["python", "File_Renamer.py", "-m", path, name])
+        dump = subprocess.run([pyver, "File_Renamer.py", "-m", path, name])
     else:
-        dump = subprocess.run(["python", "File_Renamer.py", path, name])
+        dump = subprocess.run([pyver, "File_Renamer.py", path, name])
 
 def main()-> None:
     Label(window, text="pyhton function renamer", font=("bold", 13)).grid(row=1, column=1)
@@ -33,6 +43,7 @@ def main()-> None:
     Checkbutton(window, text='movie', variable=movie, onvalue=1, offvalue=0, ).grid(row=3, column=1)
     Button(window, text="File location", command=c.fileDialog ,width=10, height=1).grid(row=3, column=3)
     Button(window, text="submit", command=submit ,width=10, height=1).grid(row=4, column=1)
+    Button(window, text="quit", command=exit ,width=10, height=1).grid(row=4, column=3)
 
 main()
 window.mainloop()
