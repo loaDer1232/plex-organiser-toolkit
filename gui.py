67e7957221ca79c7b3309a8d8ad601e7f6a11635
baksteennz <christoeras@gmail.com>
2023-09-10 21:54:21 +1200

    staeted rewrite

--- gui.py
+++ gui.py
@@ -1,4 +1,4 @@
-from tkinter import Tk, filedialog, Label, Button, Entry, Checkbutton
+from customtkinter import *
 import subprocess
 
 class GUI:
@@ -12,27 +12,38 @@ class GUI:
         return self.file_path
     
 
-window = Tk()
+window = CTk()
+window.geometry("400x300")
+set_appearance_mode("System") 
+set_default_color_theme("blue")
 c = GUI()
-movie: bool = 0
-nameEntry = Entry(window, width=7, textvariable="")
-
 
 def submit():
     path = c.getPath()
     name = nameEntry.get()
+    movie = movieBox.get()
     if movie == True:
         dump = subprocess.run(["python", "File_Renamer.py", "-m", path, name])
     else:
         dump = subprocess.run(["python", "File_Renamer.py", path, name])
 
-def main()-> None:
-    Label(window, text="pyhton function renamer", font=("bold", 13)).grid(row=1, column=1)
-    Label(window, text="enter new name:", font=("bold", 13)).grid(row=2, column=1)
-    nameEntry.grid(row=2, column=3)
-    Checkbutton(window, text='movie', variable=movie, onvalue=1, offvalue=0, ).grid(row=3, column=1)
-    Button(window, text="File location", command=c.fileDialog ,width=10, height=1).grid(row=3, column=3)
-    Button(window, text="submit", command=submit ,width=10, height=1).grid(row=4, column=1)
+movieBox = CTkCheckBox(window, text='movie')
+nameEntry = CTkEntry(window, width=120, textvariable="")
+header = CTkLabel(window, text="pyhton function renamer", font=("bold", 13))
+newName = CTkLabel(window, text="enter new name:", font=("bold", 13))
+miscOpptions = CTkLabel(window, text="addtional opptions:", font=("bold", 13))
+fileLocation = CTkButton(window, text="Select File location", command=c.fileDialog ,width=50)
+submmitButton = CTkButton(window, text="submit", command=submit ,width=50)
 
-main()
-window.mainloop()
+def main()-> None:
+    header.place(relx=0.5, anchor=N)
+    newName.place(relx=0.3, rely=0.2, anchor=CENTER)
+    nameEntry.place(relx=0.7, rely=0.2, anchor=CENTER)
+    fileLocation.place(relx=0.5, rely=0.4, anchor=CENTER)
+    miscOpptions.place(relx=0.5, rely=0.6, anchor=CENTER)
+    movieBox.place(relx=0.7, rely=0.7, anchor=CENTER)
+    submmitButton.place(relx=0.5, rely=0.9, anchor=CENTER)
+
+if __name__ == "__main__":
+    main()
+    window.mainloop()
