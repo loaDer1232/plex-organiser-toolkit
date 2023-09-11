10f1b3e21327e14c79f5f7de2593cff397cf24ed
baksteennz <christoeras@gmail.com>
2023-09-11 20:35:59 +1200

    wh7y no work

--- gui.py
+++ gui.py
@@ -1,15 +1,28 @@
+from tkinter import filedialog
 from customtkinter import *
 import subprocess
 
 class GUI:
     def __init__(self) -> None:
         self.file_path: str
+        self.movie: bool 
+        self.name: str
 
-    def fileDialog(self)-> str:
-        self.file_path = filedialog.askdirectory()
+    def fileDialog(self)-> None:
+        self.filePath = filedialog.askdirectory()
+
+    def submiter(self, movie: bool, name: str)-> None:
+        self.movie = movie
+        self.name = name
     
-    def getPath(self) -> str:
-        return self.file_path
+    def __getattribute__(self, __name: str):
+       match __name:
+            case "name":
+               return self.name
+            case "movie":
+               return self.movie
+            case "filePath":
+               return self.filePath
     
 
 window = CTk()
@@ -19,23 +32,28 @@ set_default_color_theme("blue")
 c = GUI()
 
 def submit():
-    path = c.getPath()
-    name = nameEntry.get()
-    movie = movieBox.get()
+    path = c.__getattribute__("filePath")
+    name = c.__getattribute__("name")
+    movie = c.__getattribute__("movie")
     if movie == True:
         dump = subprocess.run(["python", "File_Renamer.py", "-m", path, name])
     else:
         dump = subprocess.run(["python", "File_Renamer.py", path, name])
 
-movieBox = CTkCheckBox(window, text='movie')
-nameEntry = CTkEntry(window, width=120, textvariable="")
-header = CTkLabel(window, text="pyhton function renamer", font=("bold", 13))
-newName = CTkLabel(window, text="enter new name:", font=("bold", 13))
-miscOpptions = CTkLabel(window, text="addtional opptions:", font=("bold", 13))
-fileLocation = CTkButton(window, text="Select File location", command=c.fileDialog ,width=50)
-submmitButton = CTkButton(window, text="submit", command=submit ,width=50)
 
 def main()-> None:
+    def sub():
+        c.submiter(movieBox.get(), nameEntry.get())
+        submit()
+    
+    movieBox = CTkCheckBox(window, text='movie')
+    nameEntry = CTkEntry(window, width=120, textvariable="")
+    header = CTkLabel(window, text="pyhton function renamer", font=("bold", 13))
+    newName = CTkLabel(window, text="enter new name:", font=("bold", 13))
+    miscOpptions = CTkLabel(window, text="addtional opptions:", font=("bold", 13))
+    fileLocation = CTkButton(window, text="Select File location", command=c.fileDialog ,width=50)
+    submmitButton = CTkButton(window, text="submit", command=sub, width=50)
+
     header.place(relx=0.5, anchor=N)
     newName.place(relx=0.3, rely=0.2, anchor=CENTER)
     nameEntry.place(relx=0.7, rely=0.2, anchor=CENTER)
